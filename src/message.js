56c53251938fc32e3b3cd298209aee63e8c68116
Rafael Xavier de Souza <rxaviers@gmail.com>
2014-05-29 09:37:36 -0700

    Message: Create formatMessage
    Also s/loadMessages/loadTranslations;

--- src/message.js
+++ src/message.js
@@ -5,18 +5,34 @@ define([
 ], function( Cldr, Globalize, alwaysArray ) {
 
 /**
- * .loadMessages( json )
+ * .formatMessage( message, data )
+ *
+ * @message [Sring]
+ *
+ * @data [Array or JSON]
+ *
+ * Format message FIXME.
+ */
+Globalize.formatMessage = function( message, data ) {
+	// Replace {attribute}'s
+	message = message.replace( /{[0-9a-zA-Z-_. ]+}/g, function( name ) {
+		name = name.replace( /^{([^}]*)}$/, "$1" );
+		return data[ name ];
+	});
+	return message;
+};
+
+/**
+ * .loadTranslations( json )
  *
  * @json [JSON]
  *
- * Load messages (translation) data for default/instance locale.
+ * Load translation data.
  */
-Globalize.loadMessages =
-Globalize.prototype.loadMessages = function( json ) {
+Globalize.loadTranslations = function( json ) {
 	var customData = {
-		"globalize-messages": {}
+		"globalize-translations": json
 	};
-	customData[ "globalize-messages" ][ this.cldr.attributes.languageId ] = json;
 	Cldr.load( customData );
 };
 
@@ -29,8 +45,11 @@ Globalize.prototype.loadMessages = function( json ) {
  */
 Globalize.translate =
 Globalize.prototype.translate = function( path ) {
-	path = alwaysArray( path );
-	return this.cldr.get( [ "globalize-messages/{languageId}" ].concat( path ) );
+	path = alwaysArray( path, messageData );
+	return this.formatMessage(
+		this.cldr.get( [ "globalize-translations/{languageId}" ].concat( path ) ),
+		messageData
+	);
 };
 
 return Globalize;
